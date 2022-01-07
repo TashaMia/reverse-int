36f02439ef420f76c5aec3e60bd4507d73ac354d
Save Rights <saverights@macbook-air-save.local>
2022-01-07 13:09:41 +0000

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let arr=[];
+   
+        if(n<0){
+     n = n + (-n * 2); 
+    }
+    let str = String(n);
+    arr=str.split('');
+    arr.reverse();
+    if (arr[0]==0){
+     arr.shift(); 
+    }
+  return(arr.join(''));
 }
